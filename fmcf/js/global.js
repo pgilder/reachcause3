b5c368b48c7d704bd91862696b9a3528839e28d3
Patrick Gilder <pgilder@cyberitas.com>
2018-09-10 08:15:10 -0700

    All Files Added

---
+++ fmcf/js/global.js
@@ -0,0 +1,35 @@
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.sitelongtitle').innerHTML = 'Feed My Childrens Fund';
+
+// Using the jQuery library
+$('.sitelongtitle').html('Feed My Childrens Fund');
+
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.siteshorttitle').innerHTML = 'FMCF';
+
+// Using the jQuery library
+$('.siteshorttitle').html('FMCF');
+
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.siteaddress').innerHTML = 'PO Box 83775 Phoenix, AZ 85071, USA';
+
+// Using the jQuery library
+$('.siteaddress').html('PO Box 83775 Phoenix, AZ 85071, USA');
+
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.sitephone').innerHTML = '+1 (602) 241-2873';
+
+// Using the jQuery library
+$('.sitephone').html('+1 (602) 241-2873');
+
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.siteemail').innerHTML = 'info@cemfund.org';
+
+// Using the jQuery library
+$('.siteemail').html('info@cemfund.org');
+
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.siteshortdescription').innerHTML = 'The Childrens Emergency Medical Fund (CEMF) is a non profit charity founded by Lon Taylor in 2000. CEMF works with the Northern Arizona Food Bank, and the Borderland Food Bank, which serves charities throughout Arizona and America. CEMF is an approved 501(c)3 charity listed in publication 78';
+
+// Using the jQuery library
+$('.siteshortdescription').html('The Childrens Emergency Medical Fund (CEMF) is a non profit charity founded by Lon Taylor in 2000. CEMF works with the Northern Arizona Food Bank, and the Borderland Food Bank, which serves charities throughout Arizona and America. CEMF is an approved 501(c)3 charity listed in publication 78');
