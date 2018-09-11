ce5c7f3dad7c2a2593973999d2bdccdb9111912d
pgilder <pgilder@designdelegate.co>
2018-09-10 21:43:25 -0700

    Updated Global JS Specs

--- fmcf/js/global.js
+++ fmcf/js/global.js
@@ -1,3 +1,4 @@
+
 // 'Modern' browsers (IE8+, use CSS-style selectors)
 document.querySelector('.sitelongtitle').innerHTML = 'Feed My Childrens Fund';
 
@@ -32,4 +33,42 @@ $('.siteemail').html('info@cemfund.org');
 document.querySelector('.siteshortdescription').innerHTML = 'The Childrens Emergency Medical Fund (CEMF) is a non profit charity founded by Lon Taylor in 2000. CEMF works with the Northern Arizona Food Bank, and the Borderland Food Bank, which serves charities throughout Arizona and America. CEMF is an approved 501(c)3 charity listed in publication 78';
 
 // Using the jQuery library
-$('.siteshortdescription').html('The Childrens Emergency Medical Fund (CEMF) is a non profit charity founded by Lon Taylor in 2000. CEMF works with the Northern Arizona Food Bank, and the Borderland Food Bank, which serves charities throughout Arizona and America. CEMF is an approved 501(c)3 charity listed in publication 78');
+$('.siteshortdescription').html('Feed My Childrens Fund (FMCF) is a non profit charity founded by Lon Taylor in 2000. FMCF works with the Northern Arizona Food Bank, and the Borderland Food Bank, which serves charities throughout Arizona and America. FMCF is an approved 501(c)3 charity listed in publication 78');
+
+// COPY CONTENT
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.product').innerHTML = 'fresh food';
+
+// Using the jQuery library
+$('.product').html('fresh food');
+
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.producttype').innerHTML = 'nutritious meals';
+
+// Using the jQuery library
+$('.producttype').html('nutritious meals');
+
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.recipient').innerHTML = 'hungry children';
+
+// Using the jQuery library
+$('.recipient').html('hungry children');
+
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.recipientshort').innerHTML = 'children';
+
+// Using the jQuery library
+$('.recipientshort').html('children');
+
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.ratio').innerHTML = '1 in 5';
+
+// Using the jQuery library
+$('.ratio').html('1 in 5');
+
+
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.EIN').innerHTML = '87-1234567';
+
+// Using the jQuery library
+$('.EIN').html('87-1234567');
