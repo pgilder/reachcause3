55dbc4470431e6f087cc52f64745f984b9a133b3
Patrick Gilder <pgilder@cyberitas.com>
2018-09-12 09:25:53 -0700

    Global File Change

--- wildanimalsanctuaryfund/js/global.js
+++ wildanimalsanctuaryfund/js/global.js
@@ -1,15 +1,15 @@
 
 // 'Modern' browsers (IE8+, use CSS-style selectors)
-document.querySelector('.sitelongtitle').innerHTML = 'Feed My Childrens Fund';
+document.querySelector('.sitelongtitle').innerHTML = 'Wild Animal Sanctuary Fund';
 
 // Using the jQuery library
-$('.sitelongtitle').html('Feed My Childrens Fund');
+$('.sitelongtitle').html('Wild Animal Sanctuary Fund');
 
 // 'Modern' browsers (IE8+, use CSS-style selectors)
-document.querySelector('.siteshorttitle').innerHTML = 'FMCF';
+document.querySelector('.siteshorttitle').innerHTML = 'WASF';
 
 // Using the jQuery library
-$('.siteshorttitle').html('FMCF');
+$('.siteshorttitle').html('WASF');
 
 // 'Modern' browsers (IE8+, use CSS-style selectors)
 document.querySelector('.siteaddress').innerHTML = 'PO Box 83775 Phoenix, AZ 85071, USA';
@@ -24,10 +24,10 @@ document.querySelector('.sitephone').innerHTML = '+1 (602) 241-2873';
 $('.sitephone').html('+1 (602) 241-2873');
 
 // 'Modern' browsers (IE8+, use CSS-style selectors)
-document.querySelector('.siteemail').innerHTML = 'info@cemfund.org';
+document.querySelector('.siteemail').innerHTML = 'info@wildanimalsanctuaryfund.org';
 
 // Using the jQuery library
-$('.siteemail').html('info@cemfund.org');
+$('.siteemail').html('info@wildanimalsanctuaryfund.org');
 
 // 'Modern' browsers (IE8+, use CSS-style selectors)
 document.querySelector('.siteshortdescription').innerHTML = 'The Childrens Emergency Medical Fund (CEMF) is a non profit charity founded by Lon Taylor in 2000. CEMF works with the Northern Arizona Food Bank, and the Borderland Food Bank, which serves charities throughout Arizona and America. CEMF is an approved 501(c)3 charity listed in publication 78';
@@ -74,10 +74,10 @@ $('.ratio').html('1 in 5');
 
 
 // 'Modern' browsers (IE8+, use CSS-style selectors)
-document.querySelector('.EIN').innerHTML = '87-1234567';
+document.querySelector('.EIN').innerHTML = '82-0755123';
 
 // Using the jQuery library
-$('.EIN').html('87-1234567');
+$('.EIN').html('82-0755123');
 
 
 // 'Modern' browsers (IE8+, use CSS-style selectors)
