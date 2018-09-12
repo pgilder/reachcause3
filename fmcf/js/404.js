752f219b36fb52ce5c32d912ef282416158ed4d1
pgilder <pgilder@designdelegate.co>
2018-09-11 20:13:59 -0700

    Updated Head and Footer

---
+++ fmcf/js/404.js
@@ -0,0 +1,7 @@
+
+
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.purpose').innerHTML = 'Food Rescue';
+
+// Using the jQuery library
+$('.purpose').html('Food Rescue');
