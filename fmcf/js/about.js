9d6464d4ceedab3765700083fdac71db37ccb8d6
pgilder <pgilder@designdelegate.co>
2018-09-11 19:45:24 -0700

    Final Changes

---
+++ fmcf/js/about.js
@@ -0,0 +1,53 @@
+
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.sitelongtitle').innerHTML = 'Feed My Childrens Fund';
+
+// Using the jQuery library
+$('.sitelongtitle').html('Feed My Childrens Fund');
+
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.siteshorttitle').innerHTML = 'FMCF';
+
+// Using the jQuery library
+$('.siteshorttitle').html('FMCF');
+
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.siteaddress').innerHTML = 'PO Box 83775 Phoenix, AZ 85071, USA';
+
+// Using the jQuery library
+$('.siteaddress').html('PO Box 83775 Phoenix, AZ 85071, USA');
+
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.sitephone').innerHTML = '+1 (602) 241-2873';
+
+// Using the jQuery library
+$('.sitephone').html('+1 (602) 241-2873');
+
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.siteemail').innerHTML = 'info@cemfund.org';
+
+// Using the jQuery library
+$('.siteemail').html('info@cemfund.org');
+
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.siteshortdescription').innerHTML = 'The Childrens Emergency Medical Fund (CEMF) is a non profit charity founded by Lon Taylor in 2000. CEMF works with the Northern Arizona Food Bank, and the Borderland Food Bank, which serves charities throughout Arizona and America. CEMF is an approved 501(c)3 charity listed in publication 78';
+
+// Using the jQuery library
+$('.siteshortdescription').html('Feed My Childrens Fund (FMCF) is a non profit charity founded by Lon Taylor in 2000. FMCF works with the Northern Arizona Food Bank, and the Borderland Food Bank, which serves charities throughout Arizona and America. FMCF is an approved 501(c)3 charity listed in publication 78');
+
+// COPY CONTENT
+
+
+
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.EIN').innerHTML = '87-1234567';
+
+// Using the jQuery library
+$('.EIN').html('87-1234567');
+
+
+// 'Modern' browsers (IE8+, use CSS-style selectors)
+document.querySelector('.purpose').innerHTML = 'Food Rescue';
+
+// Using the jQuery library
+$('.purpose').html('Food Rescue');
