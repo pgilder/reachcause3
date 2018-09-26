453982b5091118ace2cbbef16b8e926ba94d985c
Patrick Gilder <pgilder@cyberitas.com>
2018-09-26 07:41:28 -0700

    Added CEMFund

---
+++ cemfund/js/global.js
@@ -0,0 +1,31 @@
+<script>
+        // 'Modern' browsers (IE8+, use CSS-style selectors)
+  document.querySelector('.sitelongtitle').innerHTML = 'Feeding AHC';
+
+  // Using the jQuery library
+  $('.sitelongtitle').html('Feeding AHC');
+
+  // 'Modern' browsers (IE8+, use CSS-style selectors)
+  document.querySelector('.siteaddress').innerHTML = 'Box 83775 Phoenix, AZ 85071, USA';
+
+  // Using the jQuery library
+  $('.siteaddress').html('Box 83775 Phoenix, AZ 85071, USA');
+
+  // 'Modern' browsers (IE8+, use CSS-style selectors)
+  document.querySelector('.sitephone').innerHTML = '+1 (602) 241-2873';
+
+  // Using the jQuery library
+  $('.sitephone').html('+1 (602) 241-2873');
+
+  // 'Modern' browsers (IE8+, use CSS-style selectors)
+  document.querySelector('.siteemail').innerHTML = 'info@feedingac.org';
+
+  // Using the jQuery library
+  $('.siteemail').html('info@feedingac.org');
+
+  // 'Modern' browsers (IE8+, use CSS-style selectors)
+  document.querySelector('.siteshortdescription').innerHTML = 'Feeding Americas Children is partnering with The 3000 Club to rescue 30 million pounds of fresh fruits and vegetables every year';
+
+  // Using the jQuery library
+  $('.siteshortdescription').html('Feeding Americas Children is partnering with The 3000 Club to rescue 30 million pounds of fresh fruits and vegetables every year');
+</script>
